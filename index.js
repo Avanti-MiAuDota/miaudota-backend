5710896a214f13aab893f59b5c922c229e7604a5
flaviare1s <flaviarster@gmail.com>
2025-10-01 10:36:10 -0300

    fix(cors): enable localhost for React frontend during development

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ ensureUploadsDir();
 
 const app = express();
 
-app.use(cors("*"));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  })
+);
 app.use(express.json());
 app.use("/api", routes);
 
