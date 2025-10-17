04574bea1e38db05f9d7d997ff297813a46d2cfe
flaviare1s <flaviarster@gmail.com>
2025-10-17 15:25:11 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global;
+const globalForPrisma = globalThis;
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "error", "warn"],
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ["error", "warn"],
     errorFormat: "pretty",
   });
+};
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
-// Log para debug
-console.log(
-  "DATABASE_URL configurada:",
-  process.env.DATABASE_URL ? "✓ SIM" : "✗ NÃO"
-);
 
 export default prisma;
